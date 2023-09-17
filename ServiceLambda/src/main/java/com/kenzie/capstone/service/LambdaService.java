3089258c3fb9debc2fe8810aab34700781cb3c88
Shamir <shamir.k.willis@gmail.com>
2023-09-17 17:56:12 -0400

    UserAccounts Lambda. Added table and stack to aws. Update the lambda files for user account usage. Still need to test. still need to implemnt within our application service after testing is finish.

--- ServiceLambda/src/main/java/com/kenzie/capstone/service/LambdaService.java
+++ ServiceLambda/src/main/java/com/kenzie/capstone/service/LambdaService.java
@@ -1,8 +1,9 @@
 package com.kenzie.capstone.service;
 
-import com.kenzie.capstone.service.model.ExampleData;
+import com.kenzie.capstone.service.convertor.UserAccountConvertor;
+import com.kenzie.capstone.service.dao.UserAccountsDao;
+import com.kenzie.capstone.service.model.*;
 import com.kenzie.capstone.service.dao.ExampleDao;
-import com.kenzie.capstone.service.model.ExampleRecord;
 
 import javax.inject.Inject;
 
@@ -12,11 +13,15 @@ import java.util.UUID;
 public class LambdaService {
 
     private ExampleDao exampleDao;
+    private UserAccountsDao userAccountsDao;
 
     @Inject
     public LambdaService(ExampleDao exampleDao) {
         this.exampleDao = exampleDao;
     }
+
+    @Inject
+    public LambdaService(UserAccountsDao userAccountsDao) { this.userAccountsDao = userAccountsDao;}
 
     public ExampleData getExampleData(String id) {
         List<ExampleRecord> records = exampleDao.getExampleData(id);
@@ -30,5 +35,34 @@ public class LambdaService {
         String id = UUID.randomUUID().toString();
         ExampleRecord record = exampleDao.setExampleData(id, data);
         return new ExampleData(id, data);
+    }
+
+    public UserAccounts getUserAccounts(String id) {
+        List<UserAccountRecord> records = userAccountsDao.getUserAccounts(id);
+        if (records.size() > 0) {
+            return new UserAccounts(records.get(0).getId(),records.get(0).getName(),records.get(0).getAccountType(),records.get(0).getPassword());
+        }
+        return null;
+    }
+
+    public UserAccounts setUserAccounts(String name, String accountType, String password) {
+        String id = UUID.randomUUID().toString();
+        userAccountsDao.setUserAccounts(id, name, accountType, password);
+        return new UserAccounts(id, name, accountType, password);
+    }
+
+    public UserAccountsResponse setUserAccounts(UserAccountsRequest request){
+        String id = UUID.randomUUID().toString();
+
+        UserAccountRecord record = new UserAccountRecord();
+
+        record = UserAccountConvertor.fromRequestToRecord(request);
+        record.setId(id);
+        userAccountsDao.storeUserAccount(record);
+
+        UserAccountsResponse response = UserAccountConvertor.fromRecordToResponse(record);
+        return response;
+
+
     }
 }
