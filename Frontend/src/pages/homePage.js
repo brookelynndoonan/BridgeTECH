cead5b43aa6760c05fcb536f6688dc7c5b41df10
meganflaherty <116700960+meganflaherty@users.noreply.github.com>
2023-09-18 15:14:19 -0500

    edited webpack.config to include homepage. added onGet method to JS

--- Frontend/src/pages/homePage.js
+++ Frontend/src/pages/homePage.js
@@ -16,4 +16,22 @@ class HomePage extends BaseClass {
 
         this.dataStore.addChangeListener(this.renderExample)
     }
+
+
+    // get login information // //or get images??
+    async onGet(event) {
+        // Prevent the page from refreshing on form submit
+        event.preventDefault();
+
+        let id = document.getElementById("login-signup").value;
+        this.dataStore.set("example", null);
+
+        let result = await this.client.getExample(id, this.errorHandler);
+        this.dataStore.set("example", result);
+        if (result) {
+            this.showMessage(`Got ${result.name}!`)
+        } else {
+            this.errorHandler("Error doing GET!  Try again...");
+        }
+    }
 }
